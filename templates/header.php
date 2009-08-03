024c08f874ec903dfe5d8e22b69a7e5089cca4d3
wlee@mozilla.com <wlee@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2009-08-03 20:39:20 +0000

    Added necessary templates for PTO
    git-svn-id: https://svn.mozilla.org/projects/pto/trunk@48461 4eb1ac78-321c-0410-a911-ec516a8615a5

---
+++ templates/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US" dir="ltr">
+  <head>
+    <title>PTO Submitted</title>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+    <script src="./js/jquery-1.3.2.min.js" type="text/javascript"></script>
+    <script src="./js/jquery-ui-1.7.2.custom.min.js" type="text/javascript"></script>
+    <script src="./js/jquery.cookie.js" type="text/javascript"></script>
+    <link rel="stylesheet" type="text/css" href="./css/style.css"/>
+    <link rel="stylesheet" type="text/css" href="./css/redmond/jquery-ui-1.7.2.custom.css"/>
+    <link rel="shortcut icon" type="image/x-icon" href="./favicon.ico" /> 
+  </head>
+
+  <body>
