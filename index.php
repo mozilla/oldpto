6f344491e2dd87aff68acfb69639f2cc2b9f74ff
wlee@mozilla.com <wlee@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2009-09-08 17:31:55 +0000

    HTML 5 tags, YEEAAAUH.
    git-svn-id: https://svn.mozilla.org/projects/pto/trunk@51037 4eb1ac78-321c-0410-a911-ec516a8615a5

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once("auth.php");
 
 $data = ldap_find(
   $connection,
-  "mail=". $_SERVER["PHP_AUTH_USER"], 
+  "mail=". $_SERVER["PHP_AUTH_USER"],
   array("cn", "manager")
 );
 $notifier_email = $_SERVER["PHP_AUTH_USER"];
@@ -40,5 +40,4 @@ require_once "./templates/edit.php";
     });
   })(jQuery);
   </script>
-
 <?php require_once "./templates/footer.php"; ?>
--- templates/header.php
+++ templates/header.php
@@ -1,20 +1,27 @@
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US" dir="ltr">
+<html lang="en-US" dir="ltr">
   <head>
     <title>Mozilla PTO</title>
     <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
     <script src="./js/jquery-1.3.2.min.js" type="text/javascript"></script>
     <script src="./js/jquery-ui-1.7.2.custom.min.js" type="text/javascript"></script>
     <script src="./js/jquery.cookie.js" type="text/javascript"></script>
-    <link rel="stylesheet" type="text/css" href="./css/style.css"/>
-    <link rel="stylesheet" type="text/css" href="./css/redmond/jquery-ui-1.7.2.custom.css"/>
-    <link rel="shortcut icon" type="image/x-icon" href="./favicon.ico" /> 
+    <link rel="stylesheet" type="text/css" href="./css/style.css" />
+    <link rel="stylesheet" type="text/css" href="./css/redmond/jquery-ui-1.7.2.custom.css" />
+    <link rel="shortcut icon" type="image/x-icon" href="./favicon.ico" />
   </head>
 
   <body>
   <div id="page">
-  <h1>PTO Notification</h1>
-  <ul id="menu">
-    <li><a href="./">Notify</a></li>
-    <li><a href="./export.php">List</a></li>
-  </ul>
+
+  <header>
+    <h1>PTO Notification</h1>
+    <nav>
+      <ul id="menu">
+        <li><a href="./">Notify</a></li>
+        <li><a href="./export.php">List</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <section>
