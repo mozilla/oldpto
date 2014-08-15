51eee332538e11bd2017d1e0e9f64898293b7a1a
Fred Wenzel <fwenzel@mozilla.com>
2014-08-15 16:33:10 -0700

    Send UTF-8 encoded email, and properly base64-ify subject line. Bug 610706.

--- submit.php
+++ submit.php
@@ -203,7 +203,13 @@ if (ENABLE_DB) {
 }
 
 if (ENABLE_MAIL) {
-  $mail_result = mail(implode(", ", $notified_people), $subject, $body, "From: ". $from);
+  $mail_headers = array(
+    'From: ' . $from,
+    'Content-Type: text/plain;charset=utf-8'
+  );
+  $enc_subject = "=?utf-8?b?" . base64_encode($subject) . "?=";
+
+  $mail_result = mail(implode(", ", $notified_people), $enc_subject, $body, implode("\r\n", $mail_headers));
 } elseif (DEBUG_ON) {
   $mail_result = FALSE;
   fb("To: ". implode(", ", $notified_people));
