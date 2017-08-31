79ada2a56e815eb8da8568454f75260c9a99d8e6
Shyam Mani <fox2mike@gmail.com>
2017-08-31 08:56:10 -0700

    Changing the health check from TCP:80 to HTTP:80/favicon.ico

--- nubis/terraform/main.tf
+++ nubis/terraform/main.tf
@@ -21,7 +21,7 @@ module "load_balancer" {
   environment  = "${var.environment}"
   account      = "${var.account}"
   service_name = "${var.service_name}"
-  health_check_target = "TCP:80" 
+  health_check_target = "HTTP:80/favicon.ico" 
 }
 
 module "dns" {
