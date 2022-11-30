4686dac6ad7fd99f509147e1991c7ff171a68e1e
Greg Cox <gcox@mozilla.com>
2022-11-30 17:57:56 +0000

    Add decom banner for EOY 2022

--- templates/header.php
+++ templates/header.php
@@ -14,22 +14,28 @@
   </head>
 
   <body>
-  <div id="page">
 
   <header>
-    <h1>PTO Notification</h1>
-    <nav>
-      <ul id="menu">
-        <li><a href="./">Notify</a></li>
-        <li><a href="./mypto.php">My PTO</a></li>
+    <div class="alert">
+      <p>Starting January 1, 2023, we are switching over to use <a href="https://www.myworkday.com/vhr_mozilla/">Workday</a> as our time off tracking system.<br>For any time off taken in 2022, please continue to submit those requests into this system.<br>For future time off requests in 2023, please refrain from submitting those requests here and submit them into Workday on January 1 or after.<br>If you have questions, reach out to peopleops@mozilla.com</p>
+    </div>
+
+    <div class="topnav">
+      <div class="topnav-brand"></div>
+
+      <div class="topnav-right">
+        <a href="./">Notify</a>
+        <a href="./mypto.php">My PTO</a>
         <?php
-          if (in_array($GLOBAL_AUTH_USERNAME, $export_users)) {
-            ?><li><a href="./export.php">List</a></li>
-       	    <li><a href="./report.php">Report</a></li><?php
-	  }
+            if (in_array($GLOBAL_AUTH_USERNAME, $export_users)) {
+              ?><a href="./export.php">List</a>
+              <a href="./report.php">Report</a><?php
+            }
         ?>
-      </ul>
-    </nav>
+      </div>
+    </div>
   </header>
+
+  <div id="page">
 
   <section>
